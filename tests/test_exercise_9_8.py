90ed12f0737781f996e1c336fbb87b37ea3bb470
Callum Firth <Callum.Firth21@imperial.ac.uk>
2023-05-30 17:23:52 +0100

    Added toml, start of new tests

--- tests/test_exercise_9_8.py
+++ tests/test_exercise_9_8.py
@@ -14,13 +14,15 @@ def sample_diff_set():
     from expressions.expressions import Symbol
     x = Symbol('x')
     y = Symbol('y')
+    Exp = Symbol('Exp')
     tests = [(2 * x + 1, 'x', 1.5, 10, 2, '0.0 * x + 1.0 * 2 + 0.0'),
              (3 * x + y, 'x', 1.5, 10, 3, '0.0 * x + 1.0 * 3 + 0.0'),
              (x * y + x / y, 'y', 3, 2, 2.25,
               '0.0 * y + 1.0 * x + (0.0 * y - x * 1.0) / y ^ 2'),
              (2 * x**3 + x**2 * y, 'x', 2, 3, 36,
               '0.0 * x ^ 3 + 3 * x ^ (3 - 1) * 1.0 * 2 + 2 * x ^'
-              ' (2 - 1) * 1.0 * y + 0.0 * x ^ 2')
+              ' (2 - 1) * 1.0 * y + 0.0 * x ^ 2'),
+              (2 * x + Exp( x ), 'x', 1.5, 10, 2, '0.0 * x + 1.0 * 2 + Exp( x )')
              ]
     return tests
 
