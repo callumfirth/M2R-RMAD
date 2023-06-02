497fe4be8745d866b32687076388c92dde6a2a02
Max Fricker <122524873+max-fricker@users.noreply.github.com>
2023-06-02 20:51:50 +0100

    Removed installing previsitor.previsitor

--- rmad/__init__.py
+++ rmad/__init__.py
@@ -5,5 +5,3 @@ from .adjointfloat import AdjointFloat, postvisitor # noqa F401
 from .dag_drawer import ExpressionGraph # noqa F401
 
 from .expression_tools import evaluate, evalpostvisitor, reverse_evaluate, previsitor, adjoint, childnodes, reversemodeAD # noqa F401
-
-from .previsitor import previsitor # noqa F401
