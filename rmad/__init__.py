e899ada9938526344d7b6d23d1b84aa4d9ca02a5
Max Fricker <122524873+max-fricker@users.noreply.github.com>
2023-06-02 20:43:39 +0100

    Changed init to make all things installable

--- rmad/__init__.py
+++ rmad/__init__.py
@@ -1,1 +1,9 @@
 from .expressions import Symbol, Number, Add, Sub, Mul, Div, Pow # noqa F401
+
+from .adjointfloat import AdjointFloat, postvisitor # noqa F401
+
+from .dag_drawer import ExpressionGraph # noqa F401
+
+from .expression_tools import evaluate, evalpostvisitor, reverse_evaluate, previsitor, adjoint, childnodes, reversemodeAD # noqa F401
+
+from .previsitor import previsitor # noqa F401
