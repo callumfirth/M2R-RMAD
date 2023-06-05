be79cf21967798e9a224aa64d756986bf05ba76d
Callum Firth <Callum.Firth21@imperial.ac.uk>
2023-06-05 18:43:29 +0100

    Removed unused imports

--- rmad/__init__.py
+++ rmad/__init__.py
@@ -1,9 +1,5 @@
 from .expressions import Symbol, Number, Add, Sub, Mul, Div, Pow # noqa F401
 
-from .adjointfloat import AdjointFloat, postvisitor # noqa F401
-
-from .dag_drawer import ExpressionGraph # noqa F401
-
 from .evaluate import evaluate, reverse_evaluate # noqa F401
 
 from .traversal import evalpostvisitor, adjointprevisitor, adjoint, symbolnodes # noqa F401
