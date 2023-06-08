a086caefe3ce6b7ee7fe005012b4b88ec5396957
Callum Firth <Callum.Firth21@imperial.ac.uk>
2023-06-08 20:28:50 +0100

    Nice 10input -> 2output example to show diff in runtime

--- rmad/reversemode.py
+++ rmad/reversemode.py
@@ -30,46 +30,68 @@ def reversemodeAD(expr, conditions):
     return symbols
 
 
-x = expressions.Symbol('x')
-y = expressions.Symbol('y')
-z = expressions.Symbol('z')
+x0 = expressions.Symbol('x0')
+x1 = expressions.Symbol('x1')
+x2 = expressions.Symbol('x2')
+x3 = expressions.Symbol('x3')
+x4 = expressions.Symbol('x4')
+x5 = expressions.Symbol('x5')
+x6 = expressions.Symbol('x6')
+x7 = expressions.Symbol('x7')
+x8 = expressions.Symbol('x8')
+x9 = expressions.Symbol('x9')
 sin = expressions.Sin()
 cos = expressions.Cos()
 exp = expressions.Exp()
 log = expressions.Log()
 #Make sure arrays are float arrays to NaN is supported
 np.random.seed(0)
-a = 2
-b = 2
-c = 3
+w0 = np.random.randn(1,100000)
+w1 = np.random.randn(1,100000)
+w2 = np.random.randn(1,100000)
+w3 = np.random.randn(1,100000)
+w4 = np.random.randn(1,100000)
+w5 = np.random.randn(1,100000)
+w6 = np.random.randn(1,100000)
+w7 = np.random.randn(1,100000)
+w8 = np.random.randn(1,100000)
+w9 = np.random.randn(1,100000)
+
 # Mess around with this to see what happens, write any expr and I.V.
-conditions = {x: a, y: b} # Note our symbol map, maps the specific x symbol exprs to the value not 'x' value/str
-expression = np.asarray([x**2 + 2*x*y, sin(x**2)])
+conditions = {x0:w0, x1:w1, x2:w2, x3:w3, x4:w4, x5:w5, x6:w6, x7:w7, x8:w8, x9:w9}  # Note our symbol map, maps the specific x symbol exprs to the value not 'x' value/str
+expression = np.asarray([log(x0**2)*x9 + 2*x1*x2*cos(x3**4), exp(x4**2)*sin(x5*x6**2)+x7*x8])
 
 # Need to fix to allow np arrays in expr instead of just in the conditions
 
 start = time.time()
 reverse = reversemodeAD(expression, conditions)
-print(f"Derivative of {expression} at {conditions} in RM: {reverse}")
+#print(f"Derivative of {expression} at {conditions} in RM: {reverse}")
 end = time.time()
 print(f"Time for RM AD:{end-start}")
 #adjoint(expression)
 
-x = expressions.Symbol('x')
-y = expressions.Symbol('y')
-z = expressions.Symbol('z')
+x0 = expressions.Symbol('x0')
+x1 = expressions.Symbol('x1')
+x2 = expressions.Symbol('x2')
+x3 = expressions.Symbol('x3')
+x4 = expressions.Symbol('x4')
+x5 = expressions.Symbol('x5')
+x6 = expressions.Symbol('x6')
+x7 = expressions.Symbol('x7')
+x8 = expressions.Symbol('x8')
+x9 = expressions.Symbol('x9')
 sin = expressions.Sin()
 cos = expressions.Cos()
 exp = expressions.Exp()
 log = expressions.Log()
-
+conditions = {x0:w0, x1:w1, x2:w2, x3:w3, x4:w4, x5:w5, x6:w6, x7:w7, x8:w8, x9:w9}
 #Need to fix fm to work with np arrays
-expression = np.asarray([x**2 + 2*x*y, sin(x**2)])
+expression = np.asarray([log(x0**2)*x9 + 2*x1*x2/cos(x3**4), exp(x4**2)/sin(x5*x6**2)+x7*x8])
+
 
 start = time.time()
-conditions = {x: a, y: b}
 forward = forwardmodevisitor(expression, conditions)
-print(f"Derivative of {expression} at {conditions} in FM: {forward}")
+#print(f"Derivative of {expression} at {conditions} in FM: {forward}")
 end = time.time()
 print(f"Time for FM AD:{end-start}")
 #adjoint(expression)
