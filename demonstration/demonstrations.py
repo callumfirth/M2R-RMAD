7b3fa78402b763c9f9fd1e30b9d1c37350929fc6
Callum Firth <Callum.Firth21@imperial.ac.uk>
2023-06-09 16:53:43 +0100

    Added more demonstations

--- demonstration/demonstrations.py
+++ demonstration/demonstrations.py
@@ -27,7 +27,7 @@ def generatew(n):
     return [np.random.randn(1, 10000) for i in range(n)]
 
 
-def timex(n):
+def timex1(n):
     x = generatex(n)
     w = generatew(n)
     conditions = dict(zip(x, w))
@@ -37,20 +37,74 @@ def timex(n):
     return [timeFM(expr, conditions), timeRM(expr, conditions)]
 
 
+def timex2(n):
+    x = generatex(n)
+    w = generatew(n)
+    conditions = dict(zip(x, w))
+    expr = 1
+    for i in range(n):
+        expr *= expressions.Sin(x[i])
+    return [timeFM(expr, conditions), timeRM(expr, conditions)]
+
+
+def timex3(n):
+    x = generatex(n)
+    w = generatew(n)
+    conditions = dict(zip(x, w))
+    expr = 1
+    for i in range(n):
+        expr *= expressions.Exp(x[i])
+    return [timeFM(expr, conditions), timeRM(expr, conditions)]
+
+
+def timex4(n):
+    x = generatex(n)
+    w = generatew(n)
+    conditions = dict(zip(x, w))
+    expr = 1
+    for i in range(n):
+        expr *= expressions.Log(x[i])
+    return [timeFM(expr, conditions), timeRM(expr, conditions)]
+
+
 def plottime(n):
     nrange = range(1, n+1)
-    timelines = np.asarray([timex(n) for n in nrange])
-    fig, ax = plt.subplots()
-    FM, = ax.plot(nrange, timelines[:, 0], label="Forward Mode")
-    RM, = ax.plot(nrange, timelines[:, 1], label="Reverse Mode")
-    ax.set_title("Plotting the time to compute derivative of fn=x_1*...*x_n")
-    ax.set_ylabel("t: Time to compute function")
-    ax.set_xlabel("n: Number of input variables")
-    ax.legend()
+    timelines1 = np.asarray([timex1(n) for n in nrange])
+    timelines2 = np.asarray([timex2(n) for n in nrange])
+    timelines3 = np.asarray([timex3(n) for n in nrange])
+    timelines4 = np.asarray([timex4(n) for n in nrange])
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2, figsize=(15,10))
+    
+    FM, = ax1.plot(nrange, timelines1[:, 0], label="Forward Mode")
+    RM, = ax1.plot(nrange, timelines1[:, 1], label="Reverse Mode")
+
+    FM, = ax2.plot(nrange, timelines2[:, 0], label="Forward Mode")
+    RM, = ax2.plot(nrange, timelines2[:, 1], label="Reverse Mode")
+
+    FM, = ax3.plot(nrange, timelines3[:, 0], label="Forward Mode")
+    RM, = ax3.plot(nrange, timelines3[:, 1], label="Reverse Mode")
+
+    FM, = ax4.plot(nrange, timelines4[:, 0], label="Forward Mode")
+    RM, = ax4.plot(nrange, timelines4[:, 1], label="Reverse Mode")
+
+    ax1.set_title("Plotting the time to compute derivative of fn_1")
+    ax2.set_title("Plotting the time to compute derivative of fn_2")
+    ax3.set_title("Plotting the time to compute derivative of fn_3")
+    ax4.set_title("Plotting the time to compute derivative of fn_4")
+
+    ax1.set_ylabel("t: Time taken to compute derivative")
+    ax3.set_ylabel("t: Time taken to compute derivative")
+    ax3.set_xlabel("n: Number of input variables")
+    ax4.set_xlabel("n: Number of input variables")
+    plt.legend()
+    fig.align_labels()
+    fig.tight_layout()
+    plt.savefig('image.pdf', bbox_inches='tight', pad_inches=0)
+
     return plt.show()
 
 
-plottime(50)
+plottime(10)
 
 
 # print(f"Time for FM AD:{timeRM(expression,conditions)}")
