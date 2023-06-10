7cf4c5dc5cd61a5da1921ad02af2ae933e41ccf3
Alexander Le Marchant <aml21@ic.ac.uk>
2023-06-10 11:13:59 +0100

    Changing taylor error to functions

--- rmad/Taylor_error.py
+++ rmad/Taylor_error.py
@@ -1,53 +1,32 @@
-from reversemode import reversemodeAD
-from traversal import evalpostvisitor
-import expressions
+from rmad.reversemode import reversemodeAD
+from rmad.traversal import evalpostvisitor
+import rmad.expressions as expressions
 import matplotlib.pyplot as plt
 import numpy as np
 
-eps = [10**-(i+1) for i in range(10)]
-result = []
-for i in range(len(eps)):
-    x = expressions.Symbol('x')
-    sin = expressions.Sin()
-    y = expressions.Symbol('y')
-    conditions = {'x': 1, 'y': 1}
-    expression = x*sin(x+y)
-    exprdelta = (x + eps[i])*sin(x + eps[i] + y)
-    Jx = evalpostvisitor(expression, symbol_map=conditions)
-    Jdeltax = evalpostvisitor(exprdelta, symbol_map=conditions)
-    dJx = reversemodeAD(expression, conditions)
+
+def taylor_error(expr, condition, eps, **kwargs):
+    condition_new = dict(condition)
+    condition_new[kwargs['var']] = condition_new[kwargs['var']] + eps
+    evalpostvisitor(expr, symbol_map=condition)
+    Jx = expression.storedvalue
+    evalpostvisitor(expr, symbol_map=condition_new)
+    Jdeltax = exprdelta.storedvalue
+    dJx = reversemodeAD(expr, conditions)
     # Using taylor series expansion find O(eps^2)
-    result.append(Jdeltax - Jx - dJx[x]*eps[i])
+    return (Jdeltax - Jx - dJx[kwargs['var']]*eps)
 
-plt.plot(np.log10(np.array(eps)), np.log10(abs(np.array(result))))
-plt.gca().invert_xaxis()
-plt.xlabel("Log10 of Epsilon")
-plt.ylabel("Log10 of Taylor error")
-plt.title("Log-Log graph of Epsilon against Taylor error")
-plt.show()
-plt.clf()
 
-eps = [10**-(i+1) for i in range(10)]
-result = []
-for i in range(len(eps)):
-    x = expressions.Symbol('x')
-    sin = expressions.Sin()
-    y = expressions.Symbol('y')
-    conditions = {'x': 1, 'y': 1}
-    expression = x*sin(x+y)
-    exprdelta = (x + eps[i])*sin(x + eps[i] + y)
-    exprnegdelta = (x - eps[i])*sin(x - eps[i] + y)
-    Jx = evalpostvisitor(exprnegdelta, symbol_map=conditions)
-    Jdeltax = evalpostvisitor(exprdelta, symbol_map=conditions)
-    dJx = reversemodeAD(expression, conditions)
-    print((Jdeltax - Jx)/(2*eps[i]))
-    # Using taylor series expansion find O(eps^2)
-    result.append((Jdeltax - Jx)/(2*eps[i]) - dJx[x])
+def taylor_error_plot(expr, condition, eps, **kwargs):  #eps is a list in this case
+    result = []
+    for e in eps:
+        result.append(taylor_error(expr, condition, e, **kwargs))
+    plt.plot(np.log10(np.array(eps)), np.log10(abs(np.array(result))))
+    plt.gca().invert_xaxis()
+    plt.xlabel("Log10 of Epsilon")
+    plt.ylabel("Log10 of Taylor error")
+    plt.title("Log-Log graph of Epsilon against Taylor error")
+    plt.show()
+    
 
-plt.plot(np.log10(abs(np.array(eps))), np.log10(abs(np.array(result))))
-plt.gca().invert_xaxis()
-plt.xlabel("Log10 of Epsilon")
-plt.ylabel("Log10 of Finite Difference error")
-plt.title("Log-Log graph of Epsilon against Finite Difference error")
-plt.show()
 
