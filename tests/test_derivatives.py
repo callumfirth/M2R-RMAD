5ed1a9c4acad42b39958329847df1e055ff46efe
Max Fricker <122524873+max-fricker@users.noreply.github.com>
2023-06-12 17:48:28 +0100

    Use parametrize on pytests for more tests

--- tests/test_derivatives.py
+++ tests/test_derivatives.py
@@ -1,14 +1,10 @@
 import pytest
 import numpy as np
 import rmad.expressions as expressions
+from random import randint
 
 try:
-    from rmad.evaluate import evaluate
-except ImportError:
-    pass
-
-try:
-    from rmad.forwardmode import forwardmodeAD, forwardevaluate
+    from rmad.forwardmode import forwardmodeAD
 except ImportError:
     pass
 
@@ -17,11 +13,6 @@ try:
 except ImportError:
     pass
 
-try:
-    from rmad.traversal import evalpostvisitor, adjointprevisitor, adjoint, \
-                            symbolnodes
-except ImportError:
-    pass
 
 def test_imports():
     from rmad.evaluate import evaluate # NoQA F401
@@ -29,30 +20,41 @@ def test_imports():
     from rmad.reversemode import reversemodeAD # NoQA F401
     from rmad.traversal import evalpostvisitor, adjointprevisitor, adjoint, \
         symbolnodes # NoQA F401
-    
-def test_RMADEx1():
+
+
+@pytest.mark.parametrize("xval,yval", [(1, 1), (np.pi, np.pi),
+                                       (randint(0, 100), randint(0, 100))])
+def test_RMADEx1(xval, yval):
     x = expressions.Symbol('x')
     sin = expressions.Sin()
     y = expressions.Symbol('y')
     expression = sin(x+y)*x
-    conditions = {x: 1, y: 1}
+    conditions = {x: xval, y: yval}
     ans = reversemodeAD(expression, conditions)
-    d1 = ans[x]
-    d2 = ans[y]
-    assert np.allclose([d1, d2], [0.4931505903, -0.4161468365])
+    assert np.allclose([ans[x], ans[y]],
+                       [np.cos(xval+yval)*xval+np.sin(xval+yval),
+                        np.cos(xval+yval)*xval])
 
-def test_FMADEx1():
+
+@pytest.mark.parametrize("xval,yval", [(1, 1), (np.pi, np.pi),
+                                       (randint(0, 100), randint(0, 100))])
+def test_FMADEx1(xval, yval):
     x = expressions.Symbol('x')
     sin = expressions.Sin()
     y = expressions.Symbol('y')
     expression = sin(x+y)*x
-    conditions = {x: 1, y: 1}
+    conditions = {x: xval, y: yval}
     ans = forwardmodeAD(expression, conditions)
-    d1 = ans[x]
-    d2 = ans[y]
-    assert np.allclose([d1, d2], [0.4931505903, -0.4161468365])
+    assert np.allclose([ans[x], ans[y]],
+                       [np.cos(xval+yval)*xval+np.sin(xval+yval),
+                        np.cos(xval+yval)*xval])
 
-def test_RMADEexample1():
+
+@pytest.mark.parametrize("xval,yval,zval", [(1, 1, 1),
+                                            (np.pi, np.pi, np.exp(1)),
+                                            (randint(0, 10), randint(0, 100),
+                                             randint(0, 100))])
+def test_RMADEexample1(xval, yval, zval):
     sin = expressions.Sin()
     exp = expressions.Exp()
     log = expressions.Log()
@@ -60,14 +62,20 @@ def test_RMADEexample1():
     y = expressions.Symbol('y')
     z = expressions.Symbol('z')
     expr = np.asarray([log(z)*exp(x**2), exp(x**2)+sin(x**2 * y)])
-    conditions = {x: 2, y: 2, z: 5}
+    conditions = {x: xval, y: yval, z: zval}
     ans = reversemodeAD(expr, conditions)
-    assert np.allclose(ans[x], [2*2*np.exp(2**2)*np.log(5), 
-                                2*2*(2*np.cos(2*2**2)+np.exp(2**2))]) and \
-        np.allclose(ans[y], [0, np.cos(2*2**2)*2**2]) and \
-        np.allclose(ans[z], [np.exp(2**2)/5, 0])
-    
-def test_FMADEexample1():
+    assert np.allclose(
+        ans[x], [2*xval*np.exp(xval**2)*np.log(zval),
+                 2*xval*(yval*np.cos(yval*xval**2)+np.exp(xval**2))]
+        ) and np.allclose(ans[y], [0, np.cos(yval*xval**2)*xval**2]) and \
+        np.allclose(ans[z], [np.exp(xval**2)/zval, 0])
+
+
+@pytest.mark.parametrize("xval,yval,zval", [(1, 1, 1),
+                                            (np.pi, np.pi, np.exp(1)),
+                                            (randint(0, 10), randint(0, 100),
+                                             randint(0, 100))])
+def test_FMADEexample1(xval, yval, zval):
     sin = expressions.Sin()
     exp = expressions.Exp()
     log = expressions.Log()
@@ -75,9 +83,10 @@ def test_FMADEexample1():
     y = expressions.Symbol('y')
     z = expressions.Symbol('z')
     expr = np.asarray([log(z)*exp(x**2), exp(x**2)+sin(x**2 * y)])
-    conditions = {x: 2, y: 2, z: 5}
+    conditions = {x: xval, y: yval, z: zval}
     ans = forwardmodeAD(expr, conditions)
-    assert np.allclose(ans[x], [2*2*np.exp(2**2)*np.log(5), 
-                                2*2*(2*np.cos(2*2**2)+np.exp(2**2))]) and \
-        np.allclose(ans[y], [0, np.cos(2*2**2)*2**2]) and \
-        np.allclose(ans[z], [np.exp(2**2)/5, 0])
+    assert np.allclose(
+        ans[x], [2*xval*np.exp(xval**2)*np.log(zval),
+                 2*xval*(yval*np.cos(yval*xval**2)+np.exp(xval**2))]
+        ) and np.allclose(ans[y], [0, np.cos(yval*xval**2)*xval**2]) and \
+        np.allclose(ans[z], [np.exp(xval**2)/zval, 0])
