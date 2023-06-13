b214ae2db3462afaa9e8431894c5c9899ff19b05
Alexander Le Marchant <16alemarchant@gmail.com>
2023-06-13 14:39:07 +0100

    adding table for taylor gradient

--- demonstration/taylor_error.py
+++ demonstration/taylor_error.py
@@ -2,6 +2,7 @@ from rmad.reversemode import reversemodeAD
 from rmad.traversal import evalpostvisitor
 import matplotlib.pyplot as plt
 import numpy as np
+import rmad.expressions as expressions
 
 
 def taylor_error(expr, condition, eps, **kwargs):
@@ -27,3 +28,33 @@ def taylor_error_plot(expr, condition, eps, **kwargs):
     plt.ylabel("Log10 of Taylor error")
     plt.title("Log-Log graph of Epsilon against Taylor error")
     return fig
+
+
+def convergence_table(expr, condition, eps, **kwargs):
+    result = []
+    for e in eps:
+        result.append(taylor_error(expr, condition, e, **kwargs))
+    result = np.array(result)
+    result_dif = result[1:]/result[:-1]
+    eps_dif = np.array(eps)[1:]/np.array(eps)[:-1]
+    grads = np.log(abs(result_dif))/np.log(abs(eps_dif))
+    points = [f"{i+1}-{i+2}" for i in range(len(eps)-1)]
+    d = dict(zip(points, grads))
+    f = open("images\\rate_of_convergence.txt", 'w')
+    f.write("{:<8} {:<15}".format('Point','Gradient'))
+    f.write("\n")
+    for point, grad in d.items():
+        f.write("{:<8} {:<15}".format(point, grad))
+        f.write("\n")
+
+
+x = expressions.Symbol('x')
+y = expressions.Symbol('y')
+sin = expressions.Sin()
+
+expr = x*sin(x + y)
+conditions = {x: 1, y: 1}
+eps = [10**(-(i+1)) for i in range(10)]
+
+print(convergence_table(expr, conditions, eps, var=x))
+
