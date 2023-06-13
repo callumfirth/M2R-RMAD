44c1de68358936e930453057eae57d067f1cea8e
Max Fricker <122524873+max-fricker@users.noreply.github.com>
2023-06-13 17:03:12 +0100

    Add negative valuest to tests

--- tests/test_derivatives.py
+++ tests/test_derivatives.py
@@ -52,7 +52,8 @@ def test_FMADEx1(xval, yval):
 
 @pytest.mark.parametrize("xval,yval,zval", [(1, 1, 1),
                                             (np.pi, np.pi, np.exp(1)),
-                                            (randint(0, 10), randint(0, 100),
+                                            (randint(-9, 10),
+                                             randint(-99, 100),
                                              randint(1, 100))])
 def test_RMADEexample1(xval, yval, zval):
     sin = expressions.Sin()
@@ -75,7 +76,8 @@ def test_RMADEexample1(xval, yval, zval):
 
 @pytest.mark.parametrize("xval,yval,zval", [(1, 1, 1),
                                             (np.pi, np.pi, np.exp(1)),
-                                            (randint(0, 10), randint(0, 100),
+                                            (randint(-9, 10),
+                                             randint(-99, 100),
                                              randint(1, 100))])
 def test_FMADEexample1(xval, yval, zval):
     sin = expressions.Sin()
